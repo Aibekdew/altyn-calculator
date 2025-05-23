9b502b5be36da6308ccd0dd25bee0bf5aac4dcb1
aibek <ajbekmannapov@gmail.com>
2025-05-23 14:34:19 +0600

    chore: remove src/app/(site) duplicate pages

--- src/app/(site)/(home)/page.tsx
+++
@@ -1,9 +0,0 @@
-// app/(site)/(home)/page.tsx
-"use client"; // ➊ добавляем
-
-import HomeSection from "@/components/pages/HomeSection";
-
-export default function HomePage() {
-  // ➋ компонент с заглавной буквы
-  return <HomeSection />;
-}
